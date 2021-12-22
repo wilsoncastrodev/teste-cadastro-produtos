b497b3965461ef7f4d7d3bdc800effe785d0325e
Wilson <wcastropaixao@gmail.com>
2021-12-22 11:30:34 -0300

    Instala e configura o Vue, o Sass e o Bootstrap

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Teste de Cadastro de Produtos</h1>
+</template>
+
+<script>
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+Vue.component('app', require('./App.vue').default); 
+
+const app = new Vue({
+    el: '#app',
+});
